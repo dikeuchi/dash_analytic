b5e972d5f6b45727693c972219c34f5590e5bf06
Daiki Ikeuchi <bass.92model@gmail.com>
2021-02-02 16:25:29 +0900

    update dc2vec modeling paramator

--- bm_analytic/modeling/doc2vec_model.py
+++ bm_analytic/modeling/doc2vec_model.py
@@ -1,17 +1,16 @@
 from numpy import nan
 import pandas as pd
 
-
-
-#Import Comanies Data
+# Import Comanies Data
 df = pd.read_csv('../bm_analytic/data/BM_DataSet_1000.csv', index_col=0)
 
+# Create Senteces
 sentences = []
-for text in df['Primary_business_line'].astype(str):
+for text in df['Full_overview'].astype(str):
     text_list = text.split(' ')
     sentences.append(text_list)
 
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(sentences)]
-model = Doc2Vec(documents, vector_size=2, window=5, min_count=1, workers=4)
+model = Doc2Vec(documents, epoch=20, vector_size=50, window=15, min_count=2, workers=4)
 model.save('doc2vec.model')
